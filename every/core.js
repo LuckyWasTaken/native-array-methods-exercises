570176085cd38b41b30b47beb890b941a90f92c0
Nikolay Tikhonov <a.for.autism1337@gmail.com>
2018-07-29 22:25:51 +0400

    Done some and every

--- every/core.js
+++ every/core.js
@@ -2,14 +2,14 @@
 // are even numbers.
 
 function allEven (input) {
-  return input;
+  return input.every((x) => !(x%2));
 };
 
 // Check to see if all elements in an array
 // are of the same type.
 
 function allSameType (input) {
-  return input;
+  return input.every((x) => typeof(x) === typeof(input[0]));
 };
 
 // Check to see if every element in the matrix is
@@ -17,15 +17,21 @@ function allSameType (input) {
 // greater than 0.
 
 function positiveMatrix (input) {
-  return input;
+  return input.every((row) => row.every((x) => x>0));
 };
 
 // Check that all items in an array are strings
 // and that they all only contain the same vowels.
 
 function allSameVowels (input) {
-  return input;
+  return input.every(sameVowels);
 };
+
+function sameVowels (word) {
+  return word.split("")
+    .filter((c) => c.match(/[eyuioa]/))
+    .every((val, _, arr) => val === arr[0]);
+}
 
 module.exports = {
   allEven,
