c4f3c3a3bfc94b02578e70657b5dcebea22fc4ee
Nikolay Tikhonov <a.for.autism1337@gmail.com>
2018-07-29 21:33:21 +0400

    Done mapping

--- map/core.js
+++ map/core.js
@@ -1,17 +1,21 @@
 function multiplyBy10 (array) {
- // your code here
+  return array.map((x) => x*10);
 };
 
 function shiftRight (array) {
- // your code here
+  //array.unshift(array.pop());  Why bother mapping when you can just do that?
+  //return array;
+  return array.map((item, i, arr) => i === 0 ? arr[arr.length-1] : arr[i-1]);
 };
 
 function onlyVowels (array) {
- // your code here
+  return array.map((str) => str.replace(/[^eyuioa]/g,""));
 };
 
+
+
 function doubleMatrix (array) {
- // your code here
+  return array.map((line) => line.map((x) => x*2));
 };
 
 module.exports = {
