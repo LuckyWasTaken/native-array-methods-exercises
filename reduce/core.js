4b74239478d303170bf23cd59ed17ed1713b1709
Nikolay Tikhonov <a.for.autism1337@gmail.com>
2018-07-29 22:17:18 +0400

    Done reducing

--- reduce/core.js
+++ reduce/core.js
@@ -1,18 +1,24 @@
 function sum (array) {
-  // your code here
+    return array.reduce((acc, val) => val + acc);
 }
 
 function productAll (array) {
-  // your code here
+    return array.reduce((acc, val) => typeof(val) === "number"
+        ? val * acc
+        : productAll(val) * acc, 1);
 }
 
 function objectify (array) {
-  // your code here
-
+    return array.reduce((acc, val) => { 
+        acc[val[0]] = val[1]
+        return acc;
+    }, {});
 }
 
 function luckyNumbers (array) {
-  // your code here
+    return array.reduce((acc, val, i) => (i === array.length - 1)
+        ? acc + `and ${val}`
+        : acc + `${val}, `, "Your lucky numbers are: ");
 }
 
 
